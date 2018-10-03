f6a99ad9c242af1fe3f3f0df9853b33e04a927a8
rkirouchenaradjou <43122543+rkirouchenaradjou@users.noreply.github.com>
2018-10-03 12:23:33 -0400

    Transaction Pojo with UUID and link to User Entity

---
+++ webapp/src/main/java/com/csye6255/web/application/fall2018/pojo/Transaction.java
@@ -0,0 +1,106 @@
+package com.csye6255.web.application.fall2018.pojo;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+
+/**
+ * @author Akilan Rajendiran
+ *
+ */
+@Entity
+@Table(name = "transaction_table")
+
+
+public class Transaction {
+
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(columnDefinition = "CHAR(32)")
+    @Id
+    private String transactionid;
+
+    @Column(name = "description")
+    private String description;
+    @Column(name = "date")
+    private String date;
+    @Column(name = "amount")
+    private String amount;
+    @Column(name = "merchant")
+    private String merchant;
+    @Column(name = "category")
+    private String category;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+
+    public Transaction() {
+    }
+
+
+    public String getTransactionid() {
+        return transactionid;
+    }
+
+    public void setTransactionid(String transactionid) {
+        this.transactionid = transactionid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getMerchant() {
+        return merchant;
+    }
+
+    public void setMerchant(String merchant) {
+        this.merchant = merchant;
+    }
+
+    public String getCatergory() {
+        return category;
+    }
+
+    public void setCatergory(String catergory) {
+        this.category = catergory;
+    }
+}
+
