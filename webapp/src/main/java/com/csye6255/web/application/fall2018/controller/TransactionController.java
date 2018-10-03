cf7d69de7da0b72023aba8e17fe07cfc0307d062
rkirouchenaradjou <43122543+rkirouchenaradjou@users.noreply.github.com>
2018-10-03 12:17:51 -0400

    Make Creation of Transaction Work

--- webapp/src/main/java/com/csye6255/web/application/fall2018/controller/TransactionController.java
+++ webapp/src/main/java/com/csye6255/web/application/fall2018/controller/TransactionController.java
@@ -4,15 +4,33 @@ import com.csye6255.web.application.fall2018.dao.TransactionDAO;
 import com.csye6255.web.application.fall2018.dao.UserDAO;
 import com.csye6255.web.application.fall2018.pojo.Transaction;
 import com.csye6255.web.application.fall2018.pojo.User;
+import com.csye6255.web.application.fall2018.utilities.AuthorizationUtility;
+import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
 
 /**
-        * @author Akilan Rajendiran
-        *
-        */
+ * @author Akilan Rajendiran
+ *
+ */
 
+@Controller
 public class TransactionController {
 
+    @Autowired
     UserDAO userDao;
+
+    @Autowired
     TransactionDAO transactionDAO;
 
     @RequestMapping(value = "/transaction", method = RequestMethod.GET, produces = "application/json")
@@ -22,12 +40,7 @@ public class TransactionController {
         JsonObject jsonObject = new JsonObject();
         final String authorization = request.getHeader("Authorization");
         if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
-            // Authorization: Basic base64credentials
-            String base64Credentials = authorization.substring("Basic".length()).trim();
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
-            // credentials = username:password
-            final String[] values = credentials.split(":", 2);
+            String[] values = AuthorizationUtility.getHeaderValues(authorization);
             String userName = values[0];
             String password = values[1];
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -44,7 +57,7 @@ public class TransactionController {
 
                     if( ((List) transactionList).size()!=0){
                         for (Transaction transaction : transactionList) {
-                            jsonObject.addProperty("id",transaction.getId());
+                            //jsonObject.addProperty("id",transaction.get());
                             jsonObject.addProperty("id",transaction.getDescription());
                             jsonObject.addProperty("id",transaction.getAmount());
                             jsonObject.addProperty("id",transaction.getDate());
@@ -72,20 +85,15 @@ public class TransactionController {
 
     }
 
-    @RequestMapping(value = "/transaction", method = RequestMethod.POST, produces = "application/json")
-    consumes = "application/json", headers = {"content-type=application/json; charset=utf-8"})
+    @RequestMapping(value = "/transaction", method = RequestMethod.POST, produces = {"application/json"},
+            consumes = "application/json", headers = {"content-type=application/json; charset=utf-8"})
     @ResponseBody
-    public String createTransactions(@RequestHeader HttpHeaders headers, HttpServletRequest request,Transaction jsonString)
+    public ResponseEntity createTransactions(HttpServletRequest request,@RequestBody Transaction transaction)
     {
         JsonObject jsonObject = new JsonObject();
         final String authorization = request.getHeader("Authorization");
         if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
-            // Authorization: Basic base64credentials
-            String base64Credentials = authorization.substring("Basic".length()).trim();
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
-            // credentials = username:password
-            final String[] values = credentials.split(":", 2);
+            String[] values = AuthorizationUtility.getHeaderValues(authorization);
             String userName = values[0];
             String password = values[1];
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -95,30 +103,37 @@ public class TransactionController {
             if (userList.size() != 0) {
                 User user = userList.get(0);
                 if (encoder.matches(password, user.getPassword())) {
-                    // jsonObject.addProperty("message", "Current Time is : "+ new Date().toString());
-                    Transaction transactionNew = new Transaction();
-                    transactionNew.setId(jsonString.getId());
-                    transactionNew.setDescription(jsonString.getDescription());
-                    transactionNew.setAmount(jsonString.getAmount());
-                    transactionNew.setDate(jsonString.getDate());
-                    transactionNew.setMerchant(jsonString.getMerchant());
-                    transactionNew.setCatergory(jsonString.getCatergory());
-                    transactionDAO.save(transactionNew);
+                    Transaction t = new Transaction();
+                    t.setTransactionid(transaction.getTransactionid());
+                    t.setDescription(transaction.getDescription());
+                    t.setAmount(transaction.getAmount());
+                    t.setDate(transaction.getDate());
+                    t.setMerchant(transaction.getMerchant());
+                    t.setCatergory(transaction.getCatergory());
+                    t.setUser(user);
+                    transactionDAO.save(t);
                     jsonObject.addProperty("message", "Transaction  Successful");
+                    return ResponseEntity.status(HttpStatus.CREATED).body(jsonObject.toString());
 
 
-                } else jsonObject.addProperty("message", "Incorrect Password");
+                } else {
+                    jsonObject.addProperty("message", "Incorrect Password");
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(jsonObject.toString());
+                }
 
 
-            } else jsonObject.addProperty("message", "User not found! - Try Logging in again");
+            } else {
+                jsonObject.addProperty("message", "User not found! - Try Logging in again");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(jsonObject.toString());
+            }
 
         }
 
-        else
-            jsonObject.addProperty("message","You are not logged in!");
+        else {
+            jsonObject.addProperty("message","You are not logged in - Provide Username and Password!");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(jsonObject.toString());
 
-
-        return jsonObject.toString();
+        }
 
     }
 
