9d627d7d903bf9f0c27808740ace0e1933ab17e6
Neha Pednekar <pednekar.n@husky.neu.edu>
2018-10-08 13:23:46 -0400

    Attachment changes in Transaction Controller & added AttachmentDAO

---
+++ webapp/src/main/java/com/csye6255/web/application/fall2018/dao/AttachmentDAO.java
@@ -0,0 +1,13 @@
+
+package com.csye6255.web.application.fall2018.dao;
+
+import com.csye6255.web.application.fall2018.pojo.Attachment;
+import com.csye6255.web.application.fall2018.pojo.Transaction;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface AttachmentDAO extends CrudRepository<Attachment, Transaction>  {
+    List<Attachment> findAttachmentsByTransaction(Transaction transaction);
+    Attachment findAttachmentByAttachmentid(String attachmentid);
+}
