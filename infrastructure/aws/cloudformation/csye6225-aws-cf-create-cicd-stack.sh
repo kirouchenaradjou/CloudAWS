bb6fc3f899518c3a87559d8ff8d77a0dfa2f7aec
kmenita <koonani.m@northeastern.edu>
2018-10-18 18:53:10 -0400

    Changes to cicd stack and json

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -3,11 +3,14 @@ CODEDEPLOYSERVICEROLENAME="CodeDeployServiceRole"
 EC2SERVICEROLENAME="EC2ServiceRole"
 CODEDEPLOYPOLICYNAME="CodeDeployPolicy"
 S3POLICYNAME="S3Policy"
-TRAVISUSER="TravisUser"
-S3BUCKETNAME="haha.test"
+TRAVISUSER=""
+S3BUCKETNAME=""
 CODEDEPLOYAPPNAME="CodeDeployApplication"
+EC2POLICYNAME="EC2PolicyName"
+AWSREGION="us-east-1"
+AWSACCOUNTID=""
 
-aws cloudformation create-stack $STACK_NAME --template-body file://csye6225-cf-ci-cd.json --parameters ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODEDEPLOYSERVICEROLENAME ParameterKey=EC2ServiceRoleName,ParameterValue=$EC2SERVICEROLENAME ParameterKey=CodeDeployPolicyName,ParameterValue=$CODEDEPLOYPOLICYNAME ParameterKey=S3PolicyName,ParameterValue=$S3POLICYNAME ParameterKey=TravisUser,ParameterValue=$TRAVISUSER ParameterKey=S3BucketName,ParameterValue=$S3BUCKETNAME ParameterKey=CodeDeployApplicationName,ParameterValue=$CODEDEPLOYAPPNAME
+aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODEDEPLOYSERVICEROLENAME ParameterKey=EC2ServiceRoleName,ParameterValue=$EC2SERVICEROLENAME ParameterKey=CodeDeployPolicyName,ParameterValue=$CODEDEPLOYPOLICYNAME ParameterKey=S3PolicyName,ParameterValue=$S3POLICYNAME ParameterKey=TravisUser,ParameterValue=$TRAVISUSER ParameterKey=S3BucketName,ParameterValue=$S3BUCKETNAME ParameterKey=CodeDeployApplicationName,ParameterValue=$CODEDEPLOYAPPNAME ParameterKey=AWSRegion,ParameterValue=$AWSREGION ParameterKey=AWSAccountID,ParameterValue=$AWSACCOUNTID ParameterKey=EC2PolicyName,ParameterValue=$EC2POLICYNAME
 
 export STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text)
 
