2726dcc397f7c7c3232796ed41665e3a65b0b1e5
kmenita <koonani.m@northeastern.edu>
2018-10-21 16:03:18 -0400

    changes in cicd stack

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -1,16 +1,17 @@
 STACK_NAME=$1
-CODEDEPLOYSERVICEROLENAME="CodeDeployServiceRole"
-EC2SERVICEROLENAME="EC2ServiceRole"
-CODEDEPLOYPOLICYNAME="CodeDeployPolicy"
-S3POLICYNAME="S3Policy"
-TRAVISUSER=""
-S3BUCKETNAME=""
+
+CODEDEPLOYEC2S3POLICYNAME="CodeDeploy-EC2-S3"
+CODEDEPLOYSERVICEROLENAME="CodeDeploySerivceRole"
+CODEDEPLOYS3BUCKETNAME="code-deploy.csye6225-fall2018-.me"
+TRAVISUSER="travis"
 CODEDEPLOYAPPNAME="CodeDeployApplication"
-EC2POLICYNAME="EC2PolicyName"
+TRAVISUPLOADTOS3POLICYNAME="Travis-Upload-To-S3"
+TRAVISCODEDEPLOYPOLICYNAME="Travis-Code-Deploy"
+CODEDEPLOYEC2SERVICEROLENAME="CodeDeployEC2ServiceRole"
 AWSREGION="us-east-1"
 AWSACCOUNTID=""
 
-aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODEDEPLOYSERVICEROLENAME ParameterKey=EC2ServiceRoleName,ParameterValue=$EC2SERVICEROLENAME ParameterKey=CodeDeployPolicyName,ParameterValue=$CODEDEPLOYPOLICYNAME ParameterKey=S3PolicyName,ParameterValue=$S3POLICYNAME ParameterKey=TravisUser,ParameterValue=$TRAVISUSER ParameterKey=S3BucketName,ParameterValue=$S3BUCKETNAME ParameterKey=CodeDeployApplicationName,ParameterValue=$CODEDEPLOYAPPNAME ParameterKey=AWSRegion,ParameterValue=$AWSREGION ParameterKey=AWSAccountID,ParameterValue=$AWSACCOUNTID ParameterKey=EC2PolicyName,ParameterValue=$EC2POLICYNAME
+aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-cicd.json --parameters ParameterKey=CodeDeployEC2ServiceRoleName,ParameterValue=$CODEDEPLOYEC2SERVICEROLENAME ParameterKey=TravisUploadtoS3PolicyName,ParameterValue=$TRAVISUPLOADTOS3POLICYNAME ParameterKey=TravisUser,ParameterValue=$TRAVISUSER ParameterKey=CodeDeployS3BucketName,ParameterValue=$CODEDEPLOYS3BUCKETNAME ParameterKey=CodeDeployApplicationName,ParameterValue=$CODEDEPLOYAPPNAME ParameterKey=AWSRegion,ParameterValue=$AWSREGION ParameterKey=AWSAccountID,ParameterValue=$AWSACCOUNTID ParameterKey=CodeDeployServiceRoleName,ParameterValue=$CODEDEPLOYSERVICEROLENAME ParameterKey=CodeDeployEC2S3PolicyName,ParameterValue=$CODEDEPLOYEC2S3POLICYNAME ParameterKey=TravisCodeDeployPolicyName,ParameterValue=$TRAVISCODEDEPLOYPOLICYNAME
 
 export STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text)
 
