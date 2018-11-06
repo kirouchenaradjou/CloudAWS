0d0166cf651893afe642bae255baa8981f0991c9
Raghavi Kirouchenaradjou <kirouchenaradjou.r@northeastern.neu.edu>
2018-11-06 16:50:54 -0500

    Test1

--- webapp/src/main/java/com/csye6255/web/application/fall2018/Fall2018Application.java
+++ webapp/src/main/java/com/csye6255/web/application/fall2018/Fall2018Application.java
@@ -2,13 +2,25 @@ package com.csye6255.web.application.fall2018;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan({"com.csye6255.web.application.fall2018.controller", "com.csye6255.web.application.fall2018.dao", "com.csye6255.web.application.fall2018service", "neu.csye6225.entity",
+        "com.csye6255.web.application.fall2018.utilities"})
 @SpringBootApplication
-public class Fall2018Application {
+@Profile({"aws","dev"})
+public class Fall2018Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Fall2018Application.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources( Fall2018Application.class );
     }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run( Fall2018Application.class, args );
+    }
+
 }
