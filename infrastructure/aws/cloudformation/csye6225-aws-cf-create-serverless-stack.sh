b37e054621ec7b6250a60326b16af08a6b2fcabb
Raghavi Kirouchenaradjou <kirouchenaradjou.r@northeastern.neu.edu>
2018-11-11 21:24:17 -0500

    ServerLess Stack

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-serverless-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-serverless-stack.sh
@@ -1,9 +1,10 @@
 STACK_NAME=$1
+LAMBDAS3BUCKET="lambda.csye6225-fall2018-.me"
 
 export lambdaRoleArn=$(aws iam list-roles --query 'Roles[*].[RoleName, Arn]' --output text | grep Lambda |awk '{print $2}')
 echo "lambdaRoleArn : ${lambdaRoleArn}"
 
-aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-serverless.json --parameters ParameterKey=LambdaRoleArn,ParameterValue=$lambdaRoleArn
+aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-serverless.json --parameters ParameterKey=LambdaRoleArn,ParameterValue=$lambdaRoleArn ParameterKey=LambdaS3Bucket,ParameterValue=$LAMBDAS3BUCKET
 
 export STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text)
 
