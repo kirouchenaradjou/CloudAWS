681e9eadd1f4857b6995a89a47bdd55278ba84cf
Raghavi Kirouchenaradjou <kirouchenaradjou.r@northeastern.neu.edu>
2018-11-13 11:51:13 -0500

    Lambda Flow

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-serverless-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-serverless-stack.sh
@@ -1,10 +1,11 @@
 STACK_NAME=$1
-LAMBDAS3BUCKET="lambda.csye6225-fall2018-.me"
-
 export lambdaRoleArn=$(aws iam list-roles --query 'Roles[*].[RoleName, Arn]' --output text | grep Lambda |awk '{print $2}')
 echo "lambdaRoleArn : ${lambdaRoleArn}"
 
-aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-serverless.json --parameters ParameterKey=LambdaRoleArn,ParameterValue=$lambdaRoleArn ParameterKey=LambdaS3Bucket,ParameterValue=$LAMBDAS3BUCKET
+export lambdaArn=$(aws lambda list-functions --query 'Functions[*].[FunctionName,FunctionArn]' --output text| grep MyLambdaFunction | awk '{print $2}')
+echo "lambdaArn : ${lambdaArn}"
+
+aws cloudformation create-stack --stack-name $STACK_NAME --capabilities "CAPABILITY_NAMED_IAM" --template-body file://csye6225-cf-serverless.json --parameters ParameterKey=LambdaRoleArn,ParameterValue=$lambdaRoleArn ParameterKey=LambdaArn,ParameterValue=$lambdaArn ParameterKey=LambdaS3Bucket,ParameterValue=$LAMBDAS3BUCKET
 
 export STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text)
 
